ec93e9e50a5e68b2aa4cf28a0a9240d5f8fb3560
Rowland Chazima <apungurowland90@gmail.com>
2026-07-23 13:40:31 +0300

    Add React Router and improve navigation

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,18 @@
-import MovieCard from "./MovieCard.jsx";
+import MovieCard from "./MovieCard";
+import movies from "../data/data";
 
-const MovieList = ({movies}) => {
-    return (
-        <div>
-            {movies.map((movie)=>(
+const MovieList = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8 ">
+      <h1 className="text-3xl font-bold mb-8 text-center"> Popular Movies</h1>
 
-            <MovieCard
-            key={movie.id}
-            movie={movie}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            />
-            ))}
-        </div>
-    )
-}
-export default MovieList
+export default MovieList;
